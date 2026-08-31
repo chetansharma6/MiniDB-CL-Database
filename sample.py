67b9435c5df5c8f9ba2c6ecbc6442104f13f6152
Chetan Sharma <reachtocms@gmail.com>
2026-08-30 20:55:05 -0400

    Simple CLI created

--- sample.py
+++ sample.py
@@ -1,24 +1,25 @@
-#Drafting a simple command-line interface to better understanding databases
+#Drafting a simple command-line interface to better understand databases
 
 def user_input():
     print("Welcome to data container platform!")
     print("Select no to exit.")
-    data = {}
+    data = []
+
     while True:
-        for user in data:
-            name = input("Enter your name: ")
-            age = int(input("Enter your age: "))
-            select = input("Would you like to continue? (yes/no): ")
-            if select.lower() == 'no':
-                print("Exiting the program.")
-                return data
-                return False
-            else:
-                continue
+        name = input("Enter your name: ")
+        age = int(input("Enter your age: "))
+        data.append({"name": name, "age": age})
+
+        select = input("Would you like to continue? (yes/no): ")
+        if select.lower() == 'no':
+            print("Exiting the program.")
+            return data
+
 
 def main():
     data = user_input()
     print("Collected Data: ", data)
+
 
 if __name__ == "__main__":
     main()
